2967403ec6c0fc54c8e4f634d10f94be56358ad3
Munwoo <fddsgt123@gmail.com>
2025-02-26 00:05:58 +0900

    ➕: tailwind css 적용
    #4

--- src/app/(routes)/battle/page.tsx
+++ src/app/(routes)/battle/page.tsx
@@ -1,3 +1,13 @@
+import React from 'react'
+import { Button } from '@/shared/components/ui/Button' // ✅ Button.tsx 임포트
+
 export default function BattlePage() {
-  return <div>Battle Page</div>
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Battle Page</h1>
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">Battle Page</h1>
+      {/* ✅ Primary 버튼 */}
+      <Button label="Primary Button" variant="primary" />
+    </div>
+  )
 }
