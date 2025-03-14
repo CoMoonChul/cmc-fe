f922fd08b1b7da6debb9ff66171a5e475a8dee6c
SeugnGi An <dkstmdrl2003@gmail.com>
2025-03-14 21:02:33 +0900

    푸쉬 테스트

--- src/app/(routes)/notice/page.tsx
+++ src/app/(routes)/notice/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import React, { useState } from 'react'
-import { useDeleteNoticeMutation, useNotices } from '@/features/notice/hooks'
+import {
+  useDeleteNoticeAllMutation,
+  useDeleteNoticeMutation,
+  useNotices,
+} from '@/features/notice/hooks'
 import NoticeCard from '@/features/notice/ui/NoticeCard'
 import { useRouter } from 'next/navigation'
-import { SelectNoticeResDTO } from '#/generate/notice/api'
+import { SelectNoticeListDTO, SelectNoticeResDTO } from '#/generate/notice/api'
+import { UseQueryResult } from '@tanstack/react-query'
 
 interface Notification {
   notiList: []
@@ -16,9 +21,13 @@ interface Notification {
 
 export default function NoticePage() {
   // const { notifications, setNotifications } = useState<Notification[]>()
-  const { data, refetch }: { Notification: Notification } = useNotices(0, 10)
+  // const { data, refetch }: { Notification: Notification } = useNotices(0, 10)
   const { mutate: deleteNotice } = useDeleteNoticeMutation()
+  const { mutate: deleteNoticeAll } = useDeleteNoticeAllMutation()
   const router = useRouter()
+  const { data, refetch }: UseQueryResult<SelectNoticeListDTO, Error> =
+    useNotices(0, 10)
+
   /**
    * 알림 삭제
    * @param id
@@ -34,9 +43,25 @@ export default function NoticePage() {
     handleDelete(id)
   }
 
+  const handleDeleteAll = () => {
+    console.log('알림 전체 삭제')
+    deleteNoticeAll()
+  }
+
   return (
     <div className="min-h-screen p-6 bg-white text-black dark:bg-black dark:text-white">
       <h1 className="text-2xl font-bold mb-4">알림함</h1>
+      <div className="flex justify-between mb-4">
+        <p className="text-gray-500">총 {data?.totalElements || 0}개의 알림</p>
+        {data?.notiList?.length > 0 && (
+          <button
+            onClick={handleDeleteAll}
+            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+          >
+            전체삭제
+          </button>
+        )}
+      </div>
 
       <div className="space-y-4">
         {data?.notiList?.length > 0 ? (
--- src/features/notice/hooks.ts
+++ src/features/notice/hooks.ts
@@ -1,6 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { NOTICE } from '#/generate'
-import { deleteNotice, selectPageNotice } from '@/entities/notification/api'
+import {
+  deleteNotice,
+  deleteNoticeAll,
+  selectPageNotice,
+} from '@/entities/notification/api'
 
 export const useNotices = (page: number, size: number) => {
   return useQuery<NOTICE.SelectNoticeListDTO>({
@@ -20,6 +24,21 @@ export const useDeleteNoticeMutation = () => {
     onError: (error) => {
       console.error('삭제 오류:', error)
       alert('알림 삭제에 실패했습니다.')
+    },
+  })
+}
+
+export const useDeleteNoticeAllMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: () => deleteNoticeAll(),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['notices']) // 삭제 후 알림 목록 새로고침
+    },
+    onError: (error) => {
+      console.error('삭제 오류:', error)
+      alert('전체 알림 삭제에 실패했습니다.')
     },
   })
 }
