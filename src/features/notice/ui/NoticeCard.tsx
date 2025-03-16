69891bd14b8f2dfe0b122f13f54100095e1eff6d
SeugnGi An <dkstmdrl2003@gmail.com>
2025-03-16 15:04:22 +0900

    푸쉬 테스트

--- src/features/notice/ui/NoticeCard.tsx
+++ src/features/notice/ui/NoticeCard.tsx
@@ -1,19 +1,3 @@
-interface Notification {
-  create_user: number | undefined
-  created_at: string
-  link_url: string
-  noti_content: string
-  noti_id: number
-  noti_template_id: number
-  noti_template_nm: string
-  noti_title: string
-  noti_type: 'NOTI' | 'JOIN' | 'LINK'
-  reason_noti: string
-  send_at: string
-  send_state: string
-  user_num: number
-}
-
 const formatDate = (date: string) => {
   console.log('dddd : ', date)
   const date2 = new Date(date.replace(' ', 'T'))
@@ -30,11 +14,29 @@ const formatDate = (date: string) => {
 }
 
 const NoticeCard = ({
-  notification,
+  noti_id,
+  user_num,
+  noti_template_id,
+  send_at,
+  send_state,
+  link_url,
+  created_at,
+  reason_noti,
+  noti_title,
+  noti_type,
   onDelete,
   onAccept,
 }: {
-  notification: Notification
+  noti_id?: number
+  user_num?: number
+  noti_template_id?: number
+  send_at?: string
+  send_state?: string
+  link_url?: string
+  created_at?: string
+  reason_noti?: string
+  noti_title?: string
+  noti_type?: string
   onDelete: (id: number) => void
   onAccept: (id: number, link: string) => void
 }) => {
@@ -43,54 +45,47 @@ const NoticeCard = ({
       {/* 삭제 버튼 */}
       <button
         className="absolute top-2 right-2 text-gray-500 hover:text-red-500 transition"
-        onClick={() => onDelete(notification.noti_id)}
+        onClick={() => onDelete(noti_id)}
       >
         X
       </button>
 
       {/* 생성 날짜 */}
       <p className="text-xs text-gray-500 dark:text-gray-400">
-        {formatDate(notification.created_at)}
+        {formatDate(created_at)}
       </p>
 
       {/* 알림 제목 */}
-      <h3 className="font-semibold">{notification.noti_title}</h3>
+      <h3 className="font-semibold">{noti_title}</h3>
 
       {/* 알림 내용 */}
-      {notification.noti_type === 'LINK' ? (
-        <a
-          href={notification.link_url}
-          className="text-blue-500 hover:underline"
-        >
-          {notification.reason_noti}
+      {noti_type === 'LINK' ? (
+        <a href={link_url} className="text-blue-500 hover:underline">
+          {reason_noti}
         </a>
       ) : (
-        <p className="text-gray-700 dark:text-gray-300">
-          {notification.reason_noti}
-        </p>
+        <p className="text-gray-700 dark:text-gray-300">{reason_noti}</p>
       )}
 
       {/* 버튼 영역 */}
-      {notification.noti_type === 'LINK' && (
+      {noti_type === 'LINK' && (
         <a
-          href={notification.link_url}
+          href={link_url}
           className="mt-2 px-4 py-2 bg-blue-500 text-white text-sm rounded-md hover:bg-blue-600 transition"
         >
           이동
         </a>
       )}
-      {notification.noti_type === 'JOIN' && (
+      {noti_type === 'JOIN' && (
         <div className="flex gap-2 mt-2">
           <button
             className="flex-1 px-4 py-2 bg-green-500 text-white text-sm rounded-md hover:bg-green-600 transition"
-            onClick={() =>
-              onAccept(notification.noti_id, notification.link_url)
-            }
+            onClick={() => onAccept(noti_id, link_url)}
           >
             수락
           </button>
           <button
-            onClick={() => onDelete(notification.noti_id)}
+            onClick={() => onDelete(noti_id)}
             className="flex-1 px-4 py-2 bg-red-500 text-white text-sm rounded-md hover:bg-red-600 transition"
           >
             거절
