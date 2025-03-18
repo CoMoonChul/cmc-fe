6c6d25fc3da4d58c7ff6e0cf7a632fd500782054
Allday Park <111589149+JongIlParks@users.noreply.github.com>
2025-03-18 23:47:29 +0900

    #45 수정작업중 LIst card 만들어야함

---
+++ src/features/review/ui/ReviewListCard.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/navigation'
+import { formatNumberWithCommas } from '@/shared/lib/number'
+import { getFormattedCreatedAt } from '@/shared/lib/date'
+
+// const ReviewListCard = ({
+
+// })
+
+// return ()
+
+// export default ReviewListCard
