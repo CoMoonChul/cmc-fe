a753cc988be7558c67e8f0d14f9215b648d9688b
KoYoungSung <ken3650@naver.com>
2025-03-27 17:42:12 +0900

    라이브코딩 PROTECTED_ROUTES URL 추가

--- src/shared/config/protectedRoutes.ts
+++ src/shared/config/protectedRoutes.ts
@@ -1,7 +1,8 @@
 // 정적 라우팅 경로
-export const PROTECTED_ROUTES = ['/battle/form', '/notice']
+export const PROTECTED_ROUTES = ['/battle/form', '/livecoding', '/livecoding/join', '/notice']
 
 // 동적 라우팅 경로
 export const DYNAMIC_PROTECTED_ROUTES = [
   /^\/battle\/form\/[a-zA-Z0-9-_]+$/, // /battle/form/:id (예: /battle/123, /battle/abc-def)
+  /^\/livecoding\/[a-zA-Z0-9-]+$/,
 ]
