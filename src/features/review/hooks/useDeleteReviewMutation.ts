1e416e726bd7aa2448f15daf74f5a4fdb6144579
박종일_FO-FE_소프트웍스 <allday@partner.sk.com>
2025-04-01 18:35:58 +0900

    ✨ 리뷰 상세 일부 수정 | 삭제 버튼 활성화

---
+++ src/features/review/hooks/useDeleteReviewMutation.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { REVIEW } from '#/generate'
+import { QUERY_KEYS } from '../types'
+import { deleteReview } from '@/entities/review/api'
+
+/**
+ * 리뷰 삭제 mutation
+ * @param reviewId 리뷰 id
+ */
+export const useDeleteReviewMutation = (reviewId: number) => {
+  const queryClient = useQueryClient()
+
+  return useMutation<
+    REVIEW.DeleteReviewResDTO,
+    Error,
+    REVIEW.DeleteReviewReqDTO
+  >({
+    mutationFn: (data) => deleteReview(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.REVIEW.LIST],
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.REVIEW.DETAIL, reviewId],
+      })
+    },
+  })
+}
