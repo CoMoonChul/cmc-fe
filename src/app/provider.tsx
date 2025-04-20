8d4d0ef53cf0d9258006d10bdaf373f3a2385635
Munwoo <fddsgt123@gmail.com>
2025-04-21 01:07:30 +0900

    ✨: 실시간 시작 버튼 추가
    #151

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,11 +1,24 @@
 'use client'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Loading from '@/shared/ui/Loading'
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+          mutations: {
+            retry: false,
+          },
+        },
+      }),
+  )
 
   return (
     <QueryClientProvider client={queryClient}>
