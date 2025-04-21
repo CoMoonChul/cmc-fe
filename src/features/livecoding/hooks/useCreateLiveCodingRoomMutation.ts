d821604b22d0d3cdaa63b3953ed5ee60209449c6
박종일_FO-FE_소프트웍스 <allday@partner.sk.com>
2025-04-21 14:48:54 +0900

    [실시간 공유] 리뷰 > 실시간 공유 연동 | 리뷰 상세에서 실시간 공유 페이지로 이동 할 수 있도록 버튼 추가, tenstackquery  추가 /  className 문법 에러 일부 수정

--- src/features/livecoding/hooks/useCreateLiveCodingRoomMutation.ts
+++ src/features/livecoding/hooks/useCreateLiveCodingRoomMutation.ts
@@ -1,50 +1,91 @@
 import { LIVECODING } from '#/generate'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-
+import { useMutation } from '@tanstack/react-query'
 import {
   createLiveCoding,
   deleteLiveCoding,
   updateLiveCodingSnippet,
 } from '@/entities/livecoding/api'
 
-export const useCreateLiveCodingRoomMutation = (
-  hostId: number,
-  code: string,
-  language: string,
-) => {
+export const useCreateLiveCodingRoomMutation = () => {
   return useMutation<
     LIVECODING.CreateLiveCodingResDTO,
     Error,
-    LIVECODING.CreateLiveCodingReqDTO
+    { hostId: number; code: string; language: string }
   >({
-    mutationFn: async (data) => {
-      const createdRoomId: string | null = null
+    mutationFn: async ({ hostId, code, language }) => {
+      let createdRoomId: string | null = null
+
       try {
-        // 1. 방 생성 API 호출
-        const createResponse = await createLiveCoding(data)
+        // 1. 방 생성
+        const createResponse = await createLiveCoding({ hostId })
+        createdRoomId = createResponse.roomId
 
         console.log(
           'updateLiveCodingSnippet req',
-          createResponse.roomId,
+          createdRoomId,
           code,
           language,
         )
 
-        // 2.
+        // 2. 스니펫 초기화
         await updateLiveCodingSnippet(
-          code, // 코드
-          createResponse.roomId,
+          code,
+          createdRoomId,
           hostId,
-          [{ op: 0, text: '' }], // 기본 diff 값 (현재 BE에서 requeied 되어 있어 임시 적용)
-          language, // 언어
-          { line: 0, ch: 0 }, // 기본 커서 위치 (현재 BE에서 requeied 되어 있어 임시 적용)
+          [{ op: 0, text: '' }],
+          language,
+          { line: 0, ch: 0 },
         )
+
         return createResponse
-      } catch (error) {
-        // 스니펫 업데이트 실패 시 방 삭제 시도
-        if (createdRoomId) await deleteLiveCoding(createdRoomId)
-        throw error
+      } catch (err) {
+        if (createdRoomId) {
+          await deleteLiveCoding({ roomId: createdRoomId })
+        }
+        throw err
       }
     },
   })
 }
+
+// export const useCreateLiveCodingRoomMutation = (
+//   hostId: number,
+//   code: string,
+//   language: string,
+// ) => {
+//   return useMutation<
+//     LIVECODING.CreateLiveCodingResDTO,
+//     Error,
+//     LIVECODING.CreateLiveCodingReqDTO
+//   >({
+//     mutationFn: async (data) => {
+//       const createdRoomId: string | null = null
+//       try {
+//         // 1. 방 생성 API 호출
+//         const createResponse = await createLiveCoding(data)
+
+//         console.log(
+//           'updateLiveCodingSnippet req',
+//           createResponse.roomId,
+//           code,
+//           language,
+//         )
+
+//         // 2.
+//         await updateLiveCodingSnippet(
+//           code, // 코드
+//           createResponse.roomId,
+//           hostId,
+//           [{ op: 0, text: '' }], // 기본 diff 값 (현재 BE에서 requeied 되어 있어 임시 적용)
+//           language, // 언어
+//           { line: 0, ch: 0 }, // 기본 커서 위치 (현재 BE에서 requeied 되어 있어 임시 적용)
+//         )
+//         return createResponse
+//       } catch (error) {
+//         // 스니펫 업데이트 실패 시 방 삭제 시도
+//         if (createdRoomId) await deleteLiveCoding(createdRoomId)
+//         throw error
+//       }
+//     },
+//   })
+// }
