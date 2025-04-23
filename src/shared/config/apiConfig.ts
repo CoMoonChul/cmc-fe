e203d8af9f50319b1b3d32a55eb7e8198540a95b
DESKTOP-RDIOBJQ\sdrom <simwoohj@gmail.com>
2025-04-23 22:26:51 +0900

    ✅ 배포 테스트3

--- src/shared/config/apiConfig.ts
+++ src/shared/config/apiConfig.ts
@@ -1,11 +1,23 @@
 import { Configuration } from '#/generate/configuration'
 import { CLIENT_BASE_PATH, SERVER_BASE_PATH } from '../lib/constants'
 
+// export function getApiConfig(): Configuration {
+//   const isClient = typeof window !== 'undefined'
+//   return new Configuration({
+//     basePath: isClient
+//       ? CLIENT_BASE_PATH // CSR: Next API proxy
+//       : SERVER_BASE_PATH, // SSR: Spring Backend
+//   })
+// }
 export function getApiConfig(): Configuration {
   const isClient = typeof window !== 'undefined'
-  return new Configuration({
-    basePath: isClient
-      ? CLIENT_BASE_PATH // CSR: Next API proxy
-      : SERVER_BASE_PATH, // SSR: Spring Backend
-  })
+  const basePath = isClient ? CLIENT_BASE_PATH : SERVER_BASE_PATH
+
+  if (isClient) {
+    console.log(`[CLIENT] basePath: ${basePath}`)
+  } else {
+    console.log(`[SERVER] basePath: ${basePath}`)
+  }
+
+  return new Configuration({ basePath })
 }
--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,5 +1,6 @@
-export const CLIENT_BASE_PATH = '/api'
-export const SERVER_BASE_PATH =
-  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
+// export const CLIENT_BASE_PATH = '/api'
+// export const SERVER_BASE_PATH =  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
+export const CLIENT_BASE_PATH = process.env.NEXT_PUBLIC_FRONTEND_URL
+export const SERVER_BASE_PATH = process.env.BACKEND_URL
 export const LOGO_PATH =
   'https://cmc-public-bucket.s3.ap-northeast-2.amazonaws.com/logos/cmc_icon.png'
