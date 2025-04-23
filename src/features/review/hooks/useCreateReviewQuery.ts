f10be5c8ffffe7f1acdc04292f426c05ba871049
박종일_FO-FE_소프트웍스 <allday@partner.sk.com>
2025-04-23 18:06:39 +0900

    [실시간 공유] 리뷰 > 실시간 공유 연동 | 리뷰 상세에서 실시간 공유 페이지로 이동 할 수 있도록 버튼 추가, tenstackquery  추가 /  등록 후 캐시 초기화

--- src/features/review/hooks/useCreateReviewQuery.ts
+++ src/features/review/hooks/useCreateReviewQuery.ts
@@ -1,16 +1,24 @@
 import { REVIEW } from '#/generate'
 import { createReview } from '@/entities/review/api'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { QUERY_KEYS } from '../types'
 
 /*
  * 리뷰 생성
  */
 export const useCreateReviewQuery = () => {
+  const queryClient = useQueryClient()
+
   return useMutation<
     REVIEW.CreateReviewResDTO,
     Error,
     REVIEW.CreateReviewReqDTO
   >({
     mutationFn: (data) => createReview(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.REVIEW.LIST],
+      })
+    },
   })
 }
