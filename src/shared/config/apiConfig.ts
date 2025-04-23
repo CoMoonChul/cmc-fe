3c2b04a611704854baf981150cb262fa8a3bd961
DESKTOP-RDIOBJQ\sdrom <simwoohj@gmail.com>
2025-04-24 03:09:53 +0900

    ✅ 원래 파일로 복구

--- src/shared/config/apiConfig.ts
+++ src/shared/config/apiConfig.ts
@@ -1,19 +1,11 @@
 import { Configuration } from '#/generate/configuration'
 import { CLIENT_BASE_PATH, SERVER_BASE_PATH } from '../lib/constants'
 
-// export function getApiConfig(): Configuration {
-//   const isClient = typeof window !== 'undefined'
-//   return new Configuration({
-//     basePath: isClient
-//       ? CLIENT_BASE_PATH // CSR: Next API proxy
-//       : SERVER_BASE_PATH, // SSR: Spring Backend
-//   })
-// }
 export function getApiConfig(): Configuration {
   const isClient = typeof window !== 'undefined'
-  const basePath = isClient ? CLIENT_BASE_PATH : SERVER_BASE_PATH
-
-  console.log(`[getApiConfig] basePath: ${basePath} (isClient: ${isClient})`)
-
-  return new Configuration({ basePath })
+  return new Configuration({
+    basePath: isClient
+      ? CLIENT_BASE_PATH // CSR: Next API proxy
+      : SERVER_BASE_PATH, // SSR: Spring Backend
+  })
 }
