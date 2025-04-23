732f2aee7af51608aceb354de3befeaaf75d74eb
DESKTOP-RDIOBJQ\sdrom <simwoohj@gmail.com>
2025-04-24 03:32:15 +0900

    ✅ 배포 관련 최종 소스 수정

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,10 +1,6 @@
-// export const CLIENT_BASE_PATH = '/api'
-// export const SERVER_BASE_PATH =  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
-export const CLIENT_BASE_PATH = process.env.NEXT_PUBLIC_CLIENT_BASE_PATH
-export const SERVER_BASE_PATH = process.env.NEXT_PUBLIC_SERVER_BASE_PATH
-
-console.log('[constants] CLIENT_BASE_PATH =', CLIENT_BASE_PATH)
-console.log('[constants] SERVER_BASE_PATH =', SERVER_BASE_PATH)
+export const CLIENT_BASE_PATH = process.env.NEXT_PUBLIC_FRONTEND_URL
+export const SERVER_BASE_PATH =
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
 
 export const LOGO_PATH =
   'https://cmc-public-bucket.s3.ap-northeast-2.amazonaws.com/logos/cmc_icon.png'
